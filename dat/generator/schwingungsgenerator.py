f85861d693f188c633d1952c0d7952039a5441fc
WhiteGobo <richi.fechner@web.de>
2016-10-16 11:59:04 +0200

    added timedissolve potential

--- dat/generator/schwingungsgenerator.py
+++ dat/generator/schwingungsgenerator.py
@@ -31,3 +31,11 @@ for n in range(2,4):
     yarray = Amplitude[n] * ((Radius[n]/xarray)**12 - 2*(Radius[n]/xarray)**6) -0.5*xarray
     makesafefile_cond1(xarray, yarray, name="lennardetfeder" + str(n) + ".dat")
 
+
+for n in range(2,4):
+    tarray = np.linspace(0,2*np.pi,100)
+    xtarray = 5 + 2*np.sin(tarray)
+    yarray = Amplitude[n] * ((Radius[n]/xtarray)**12 - 2*(Radius[n]/xtarray)**6) -0.5*xtarray
+    makesafefile(tarray, yarray, name="lennardetfedertime" + str(n) + ".dat")
+    makesafefile(tarray, xtarray, name="lennardetfedertime_osci" + str(n) + ".dat")
+
